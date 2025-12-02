21ff26614a3b1fc05f8bd527c7d16d99683e8761
Maria Jose Serrano Higueras <mariajoseserranohigueras@MacBook-Air-de-Maria-Jose.local>
2025-12-02 20:39:38 +0100

    inputs added to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,37 @@ import "./styles/App.scss";
 function App() {
   return (
     <div>
+      {/* CABECERA */}
       <header className="header">
-        <h1 className="title">Listado</h1>
+        <h1 className="title">Listado de personajes</h1>
       </header>
+
+      {}
       <main className="main">
-        <form>
+        <form className="form">
+          {/* INPUT 1 → BUSCAR POR PERSONAJE */}
+          <label htmlFor="filter" className="label">
+            Busca por personaje:
+          </label>
           <input
             type="text"
             name="filter"
             id="filter"
             className="input"
-            placeholder="Escribe para filtrar"
+            placeholder="Ej.: Harry, Hermione..."
           />
+
+          {/* INPUT 2 → SELECCIONAR CASA */}
+          <label htmlFor="house" className="label">
+            Selecciona la casa:
+          </label>
+          <select id="house" name="house" className="input">
+            <option value="">Todas</option>
+            <option value="Gryffindor">Gryffindor</option>
+            <option value="Slytherin">Slytherin</option>
+            <option value="Ravenclaw">Ravenclaw</option>
+            <option value="Hufflepuff">Hufflepuff</option>
+          </select>
         </form>
       </main>
     </div>
