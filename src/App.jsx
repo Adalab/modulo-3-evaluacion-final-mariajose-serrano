666b92d1953d1609aa6fa509bc31a68acea5a9ee
Maria Jose Serrano Higueras <mariajoseserranohigueras@MacBook-Air-de-Maria-Jose.local>
2025-12-03 09:36:06 +0100

    Filter form with search input & house select added

--- src/App.jsx
+++ src/App.jsx
@@ -25,4 +25,43 @@ function App() {
     // filtro por casa (si no hay casa seleccionada, mostramos todos)
     .filter((char) => (house === "" ? true : char.house === house));
 
-  
+  return (
+    <div>
+      <header className="header">
+        <h1 className="title">Listado de personajes</h1>
+      </header>
+
+      <main className="main">
+        <form className="form">
+          {/* Input 1: filtro por nombre */}
+          <label htmlFor="filter" className="label">
+            Busca por personaje:
+          </label>
+          <input
+            type="text"
+            name="filter"
+            id="filter"
+            className="input"
+            placeholder="Escribe para filtrar"
+            value={search}
+            onChange={(event) => setSearch(event.target.value)}
+          />
+
+          {/* Input2: filtro por casa */}
+          <label htmlFor="house" className="label">
+            Selecciona la casa:
+          </label>
+          <select
+            id="house"
+            name="house"
+            className="input"
+            value={house}
+            onChange={(event) => setHouse(event.target.value)}
+          >
+            <option value="">Todas</option>
+            <option value="Gryffindor">Gryffindor</option>
+            <option value="Slytherin">Slytherin</option>
+            <option value="Ravenclaw">Ravenclaw</option>
+            <option value="Hufflepuff">Hufflepuff</option>
+          </select>
+        </form>
