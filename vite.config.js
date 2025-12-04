fb1a61f7f6fc427472f90b9404df9af90b7692e7
Maria Jose Serrano Higueras <mariajoseserranohigueras@MacBook-Air-de-Maria-Jose.local>
2025-12-04 17:27:57 +0100

    fixing vite config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
-  server: {
-    open: "/",
-    watch: {
-      usePolling: true,
-    },
+  base: "/modulo-3-evaluacion-final-mariajose-serrano/", // 👈 MUY IMPORTANTE
+  build: {
+    outDir: "docs", // para que Pages use /docs directamente
   },
 });
